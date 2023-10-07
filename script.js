90a1586ef4eb55860c031f7297d38f7e1d754c2b
snejlime <38116855+limesnej@users.noreply.github.com>
2023-10-07 22:21:07 +0200

    Add random rock, paper or scissor function

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function myFun () {
-    document.getElementById("demo").innerHTML = "Fu"
+let arr = ["scissor", "rock", "paper"];
+
+
+function getComputerChoice() {
+    // should return rock, paper or scissor
+    //Math.random() random number between 
+    // multiply it by arraylength to get the numbers between 0 -arraylength
+    // math.floor to get the index ranging from 0 to arraylength -1, since length is 3 and index should be 0 to 2
+    return arr[Math.floor((Math.random() * arr.length))]
 }
-console.log("Hello world!");
+
+console.log(getComputerChoice())
